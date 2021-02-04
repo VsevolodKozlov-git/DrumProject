a580c85996307bdc77946f7542d20f264a36c24e
VsevolodKozlov-git <71010414+VsevolodKozlov-git@users.noreply.github.com>
2021-02-04 17:12:36 +0300

    finished drum project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var drumObjects = document.querySelectorAll(".drum"); 
 
 
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
 class SoundFabric {
     constructor(path) {
         this.path = path;
@@ -19,11 +21,10 @@ class SoundFabric {
             this.audio.pause();
         }
         this.audio = null;
-    };
-
+    }; 
 }
 
-function createDict(){
+function createKeySoundDict(){
     let audioNames = [
         "tom-1.mp3","tom-2.mp3", "tom-3.mp3", "tom-4.mp3",
         "crash.mp3","kick-bass.mp3","snare.mp3"
@@ -37,36 +38,55 @@ function createDict(){
     return  keySoundDict
 }
 
-keySoundDict = createDict();
+keySoundDict = createKeySoundDict();
 
-drumObjects.forEach(element => {
-    let key = element.innerText;
-    let sound = keySoundDict[key];
-    element.addEventListener("click", function() {
-        sound.stop();
-        sound.play();
+function createTimerCntDict() {
+    res = {}
+    let keys = ['w', 'a', 's', 'd', 'j', 'k', 'l'];
+    keys.forEach(key => {
+        res[key] = 0;
     });
-});
+    return res
+}
 
-document.addEventListener("keydown", function(event) {
-    sound =  keySoundDict[event.key];
-    sound.stop();
-    sound.play();
- 
-})
+timerCntDict = createTimerCntDict();
 
 
-// for (let index = 0; index < drumObjects.length; index++) {
-//     const element = drumObjects[index];
-//     element.addEventListener("click", 
-//     function (){
-//         audioNames[index].play();
-//     });
-// }
+async function pressedButtonEffect(htmlElement) {
+    const key = htmlElement.innerText;
+    console.log(key);
+    htmlElement.classList.add("pressed");
+    timerCntDict[key]++;
+    await sleep(2000);
+    timerCntDict[key]--;
+    if (timerCntDict[key] === 0){
+        htmlElement.classList.remove("pressed");
+    }
+    
+}
 
-// document.addEventListener("keydown", function(event){
-//     let kCode = event.key;
-//     console.log("." + kCode);
-//     document.querySelector("." + kCode).click();
-   
-// })
+
+function addAllEventListenrs(){
+
+    //touch 
+    const drumObjects = document.querySelectorAll(".drum"); 
+    drumObjects.forEach(element => {
+        let key = element.innerText;
+        let sound = keySoundDict[key];
+        element.addEventListener("click", function() {
+            pressedButtonEffect(this);
+            sound.stop();
+            sound.play();
+        });
+    });
+
+    document.addEventListener("keydown", function(event) {
+        document.querySelector("." + event.key).click()
+    })
+}
+
+
+
+
+
+
